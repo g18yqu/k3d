362e62f638217d00fcca913506445a4a365478e0
Andrew N <git@hb2r5y.xyz>
2020-11-27 22:20:42 +0000

    Add extensions

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -9,3 +9,5 @@ echo 'alias k=kubectl' >>$HOME/.zshrc
 echo 'complete -F __start_kubectl k' >> $HOME/.zshrc
 echo "export PROMPT_COMMAND='history -a'" >> $HOME/.zshrc 
 echo "export HISTFILE=/commandhistory/.zsh_history" >> $HOME/.zshrc 
+git config --global user.name "Andrew N."
+git config --global user.email git@hb2r5y.xyz
